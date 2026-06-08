042aecaa9d904972661d3548314324d6b65a18cd
onuralpszr <onuralpszr@users.noreply.github.com>
2026-06-08 03:53:10 +0000

    deploy: 40193259fa9ee93e1ed195ea60a59aafa4b53587

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5a4a5fdd.js',
+                path_to_root + 'searchindex-ac227132.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
