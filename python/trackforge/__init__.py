2c853cea53ed174e0e2a2796e088631c3da485e0
Jeff Zhang <e8035669@gmail.com>
2026-08-22 09:07:49 +0800

    fix: use maturin mixed project layout for the python package
    Signed-off-by: Jeff Zhang <e8035669@gmail.com>
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

---
+++ python/trackforge/__init__.py
@@ -0,0 +1,5 @@
+from .trackforge import *  # noqa: F403
+
+__doc__ = trackforge.__doc__  # noqa: F405
+if hasattr(trackforge, "__all__"):  # noqa: F405
+    __all__ = trackforge.__all__  # noqa: F405
